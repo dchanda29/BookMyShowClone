8bdd64361a6b70ab16e4910933cbfd9967eb62dd
dchanda29 <devyanichanda29@gmail.com>
2021-07-15 01:46:32 +0530

    App index and default modified

--- src/layouts/Default.layout.jsx
+++ src/layouts/Default.layout.jsx
@@ -2,16 +2,15 @@ import React from "react";
 
 //components
 import Navbar from "../components/Navbar/navbar.component";
-
 import HeroCarousal from "../components/HeroCarousal/HeroCarousal.component";
-import HomePage from "../pages/Home.page";
+
 
 
 const DefaultLayout=(props)=>{
 
     return ( <>
            <Navbar/>
-           <HomePage/>
+           <HeroCarousal/>
             {props.children}
 
         </>
