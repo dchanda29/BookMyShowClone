27c082ca48322fb765109f7f2681772a99dc7bad
dchanda29 <devyanichanda29@gmail.com>
2021-07-18 02:22:28 +0530

    added plays page and cast details

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import MovieHOC from "./HOC/Movie.HOC";
 //Pages
 import HomePage from "./pages/Home.page";
 import Movie from "./pages/Movie.page";
+import Plays from "./pages/Plays.page";
 
 
 // Import css files
@@ -20,6 +21,7 @@ function App() {
   <>
   <DefaultHOC path="/" exact component ={HomePage}/>
   <MovieHOC path="/movie/:id" exact component={Movie} />
+  <DefaultHOC path="/plays" exact component={Plays} />
   </>
   
   ); 
--- src/components/HeroCarousal/HeroCarousal.component.jsx
+++ src/components/HeroCarousal/HeroCarousal.component.jsx
@@ -1,70 +1,67 @@
 import React from "react";
 import HeroSlider from "react-slick";
 
+// Compoenent
+import { NextArrow, PrevArrow } from "./Arrows.component";
 
-
-//component
-import {NextArrow,PrevArrow } from "./Arrows.component";
-
-
-const HeroCarousal=()=>{
-
-  const settingsLg={
-    arrows:true, 
-    autoplay:true,
-    centerMode:true,
-    centerPadding:"300px",
-    slidesToShow:1,
-    infinite:true,
-    slidesToScroll:1,
-    nextArrow:<NextArrow/>,
-    prevArrow:<PrevArrow/>
+const HeroCarousal = () => {
+  const settingsLG = {
+    arrows: true,
+    autoplay: true,
+    centerMode: true,
+    centerPadding: "300px",
+    slidesToShow: 1,
+    infinite: true,
+    slidesToScroll: 1,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
   };
 
-    const settings = {
-        arrows:true,
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        nextArrow:<NextArrow/>,
-        prevArrow:<PrevArrow/>
-      };
+  const settings = {
+    arrows: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+  };
 
-      const images = [
-        "https://images.unsplash.com/photo-1503160865267-af4660ce7bf2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
-        "https://images.unsplash.com/photo-1463592177119-bab2a00f3ccb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
-        "https://images.unsplash.com/photo-1470225620780-dba8ba36b745?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
-        "https://images.unsplash.com/photo-1493225457124-a3eb161ffa5f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
-        "https://images.unsplash.com/photo-1523473125050-1c9405e8b208?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80",
-        "https://images.unsplash.com/photo-1583504491400-a6270df17ebc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
-      ];
-    
+  const images = [
+    "https://images.unsplash.com/photo-1593642531955-b62e17bdaa9c?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "https://images.unsplash.com/photo-1626128665085-483747621778?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=610&q=80",
+    "https://images.unsplash.com/photo-1622495894321-010d1e6db2a9?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2775&q=80",
+    "https://images.unsplash.com/photo-1626050954744-92bf034ce476?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80",
+    "https://images.unsplash.com/photo-1626074961596-cab914d9392e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=664&q=80",
+  ];
 
-    return (
-        <>
-        <div className="lg:hidden">
-        <HeroSlider{...settings}>
-        {images.map((image)=>(
-            <div className="w-full h-56 md:h-80 py-3">
-            <img src={image} alt="testing" className="w-full h-full"></img>
+  return (
+    <>
+      <div className="lg:hidden">
+        <HeroSlider {...settings}>
+          {images.map((image) => (
+            <div className="w-full h-56 md:h-80 py-3 ">
+              <img src={image} alt="testing" className="w-full h-full" />
             </div>
-        ))}
+          ))}
         </HeroSlider>
-        </div>
+      </div>
 
-        <div className="hidden lg:block">
-        <HeroSlider{...settingsLg}>
-        {images.map((image)=>(
+      <div className="hidden lg:block">
+        <HeroSlider {...settingsLG}>
+          {images.map((image) => (
             <div className="w-full h-96 px-2 py-3">
-            <img src={image} alt="testing" className="w-full h-full rounded-md"></img>
+              <img
+                src={image}
+                alt="testing"
+                className="w-full h-full rounded-md"
+              />
             </div>
-        ))}
+          ))}
         </HeroSlider>
-        </div>
-        </>
-    );
+      </div>
+    </>
+  );
 };
 
 export default HeroCarousal;
